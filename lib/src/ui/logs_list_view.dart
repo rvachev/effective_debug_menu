92036f866ae5a19459532b64a3230a34248cc96f
Roman Rvachev <romandauria@gmail.com>
2023-03-06 17:16:33 +0600

    [+] Added ability to copy log to clipboard

--- lib/src/ui/logs_list_view.dart
+++ lib/src/ui/logs_list_view.dart
@@ -1,6 +1,8 @@
 import 'package:effective_debug_menu/src/extensions/log_message_type_x.dart';
 import 'package:effective_dio_interceptor/effective_dio_interceptor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class LogsListView extends StatelessWidget {
   final List<LogMessage> logs;
@@ -29,11 +31,22 @@ class _LogItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: log.type.color,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(log.toString()),
+    return Material(
+      child: InkWell(
+        onLongPress: () async {
+          await Clipboard.setData(ClipboardData(text: log.toString()));
+          Fluttertoast.showToast(
+            msg: 'Log has been copied to clipboard',
+            toastLength: Toast.LENGTH_LONG,
+          );
+        },
+        child: Container(
+          color: log.type.color,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(log.toString()),
+          ),
+        ),
       ),
     );
   }
